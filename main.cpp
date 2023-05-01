93aa003413503a814a9907664b1152f7c7456da7
llz-lian <ia1943@163.com>
2023-05-01 23:54:08 +0800

    daemon

--- main.cpp
+++ main.cpp
@@ -1,11 +1,17 @@
 #include"include/Server.hpp"
 #include"include/http/HttpProtocol.hpp"
 
-
+	
+#include <sys/types.h>
+#include <unistd.h>
 
 
 int main()
 {
+    if(::fork()>0)
+        return 0;
+    ::setsid();
+    ::umask(0);
     auto proto = Http("HTTP");
     IoUringServer server(DEFALUT_PORT,&proto);
     server.run();
